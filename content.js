c3494061789fcd2ca9a651e731f909ec2781b943
Jungbin Kim <mail@jungbin.kim>
2019-01-31 21:56:39 +0900

    Add logic for merging with same tasks

--- content.js
+++ content.js
@@ -1,14 +1,38 @@
-const days = document.getElementsByClassName('tracker__finished--list')
-for (var i = 0; i < days.length; i++) {
-  const day = days[i]
-  const date = day.getElementsByClassName('tracker__list--date-label')[0].innerHTML
-  const total = day.getElementsByClassName('tracker__list--duration')[0].innerHTML
-  console.log(`${date} Total: ${total}`)
-  const items = day.getElementsByClassName('tracker__finished--time-entries')
-  for (var j = 0; j < items.length; j++) {
-    const item = items[j]
-    const title = item.getElementsByClassName('tracker__time__entry--input')[0].getAttribute('title')
-    const duration = item.getElementsByClassName('tracker__time__entry--duration')[0].value
-    console.log(title, duration)
+function mergeSameTaskByDay() {
+  const result = []
+  const days = document.getElementsByClassName('tracker__finished--list')
+  for (var i = 0; i < days.length; i++) {
+    const day = days[i]
+    const date = day.getElementsByClassName('tracker__list--date-label')[0].innerHTML
+    const total = day.getElementsByClassName('tracker__list--duration')[0].innerHTML
+    // console.log(`${date} Total: ${total}`)
+    const taskObjectsOnDay = {
+      date,
+      total,
+      tasks: {}
+    }
+    const tasks = day.getElementsByClassName('tracker__finished--time-entries')
+    for (var j = 0; j < tasks.length; j++) {
+      const task = tasks[j]
+      const title = task.getElementsByClassName('tracker__time__entry--input')[0].getAttribute('title')
+      const duration = task.getElementsByClassName('tracker__time__entry--duration')[0].value
+
+      if (taskObjectsOnDay.tasks[title] === undefined) {
+        taskObjectsOnDay.tasks[title] = {
+          durations: [duration]
+        }
+      } else {
+        taskObjectsOnDay.tasks[title].durations.push(duration)
+      }
+      // console.log(title, duration)
+    }
+    result.push(taskObjectsOnDay)
   }
+  return result
 }
+// Get ready to receive a command
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action == 'mergeSameTaskByDay') {
+    sendResponse(mergeSameTaskByDay())
+  }
+})
--- popup.js
+++ popup.js
@@ -1,9 +1,33 @@
 document.querySelector('#test').addEventListener('click', onclickCalculatorBtn)
 
+// 로딩된 페이지를 대상으로 스크립트 실행하기 위함.
+chrome.tabs.executeScript(
+  null,
+  {
+    // 코드를 string 리터럴로 실행
+    // code:'var bodyText = document.querySelector("body").innerText;alert(bodyText);',
+    // 코드를 자바스크립트 파일로 실행
+    file: 'content.js'
+  },
+  result => {
+    // CallBack after file executed
+    // console.log(result)
+  }
+)
+
 function onclickCalculatorBtn() {
-  // 로딩된 페이지를 대상으로 스크립트 실행하기 위함.
-  chrome.tabs.executeScript(null, {
-    // code:'var bodyText = document.querySelector("body").innerText;alert(bodyText);', // 코드를 string 리터럴로 실행
-    file: 'content.js' // 코드를 자바스크립트 파일로 실행
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'mergeSameTaskByDay' }, function(response) {
+      //   console.log(response, _.sumBy)
+      _.map(_.reverse(response), tasksOnDay => {
+        Object.keys(tasksOnDay.tasks).map(taskName => {
+          const durations = tasksOnDay.tasks[taskName].durations
+          const totalDuration = durations.slice(1).reduce((prev, cur) => moment.duration(cur).add(prev), moment.duration(durations[0]))
+          console.log(`${taskName}'s total time is: ${moment.utc(totalDuration.asMilliseconds()).format('HH:mm:ss')}`)
+          return totalDuration
+        })
+        // 시간 합 Reference: https://medium.com/@vladbezden/how-to-calculate-total-time-using-moment-js-3cd79345056f
+      })
+    })
   })
 }
