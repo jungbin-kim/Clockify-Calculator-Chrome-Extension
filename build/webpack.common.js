be658e9377d41d77f00b182a892a0c7136bc3058
Jungbin Kim <mail@jungbin.kim>
2019-02-10 00:39:16 +0900

    #2  Decrease build file volume

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -2,21 +2,12 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  // 빌드 시 접근점. 파일을 두개로 빌드하고 싶은 경우, 접근점을 두개로 나누어서 할 수 있다.
-  entry: {
-    popup: './src/index.js', // Chrome extension icon 눌렀을 때 뜨는 UI에 대한 JS.
-    content: './src/content.js' // 파싱 대상(여기서는 Clockify 대시보드 페이지)에서 작동하는 JS. (Extension JS와는 Chrome에서 정의한 메시징으로 통신.)
-  },
-  // 빌드 결과물 config
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[name].js'
-  },
-  plugins: [
+module.exports = env => {
+  const plugins = [
     // index.html을 템플릿화 할 수 있는 플러그인
     new HtmlWebpackPlugin({
       title: 'Clockify Calculator', // index.html의 tilte element 내에 들어갈 텍스트
@@ -38,6 +29,31 @@ module.exports = {
     // dist 폴더 초기화 플러그인
     new CleanWebpackPlugin('dist', {
       root: path.resolve(__dirname, '..') // 대상 폴더 접근 root 지정
-    })
+    }),
+    // Moment.js library locale 요소 제외할 수 있는 플러그인
+    new MomentLocalesPlugin(),
+    // Lodash library tree shaking 플러그인
+    new LodashModuleReplacementPlugin()
   ]
+
+  if (env !== undefined && env.DO_ANALYZER === 'true') {
+    plugins.push(
+      // 번들링된 파일들에 대한 구성요소 분석 플러그인
+      new BundleAnalyzerPlugin()
+    )
+  }
+
+  return {
+    // 빌드 시 접근점. 파일을 두개로 빌드하고 싶은 경우, 접근점을 두개로 나누어서 할 수 있다.
+    entry: {
+      popup: './src/index.js', // Chrome extension icon 눌렀을 때 뜨는 UI에 대한 JS.
+      content: './src/content.js' // 파싱 대상(여기서는 Clockify 대시보드 페이지)에서 작동하는 JS. (Extension JS와는 Chrome에서 정의한 메시징으로 통신.)
+    },
+    // 빌드 결과물 config
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: '[name].js'
+    },
+    plugins
+  }
 }
